5ee4bffe1f76d57df5d00b6af3001ddc369b224d
bruno <brbaricitrin@gmail.com>
2024-06-09 21:58:07 -0300

    criado metodo listar produtos

--- src/projetoPOO/Produto.java
+++ src/projetoPOO/Produto.java
@@ -6,71 +6,87 @@ import java.util.List;
 import javax.swing.JOptionPane;
 
 public class Produto {
-	
-private String id,nomeProduto;
-private double valorProduto;
-private List listaDeProdutosDisponiveis = new ArrayList();
 
+	private String id, nomeProduto;
+	private double valorProduto;
+	private List listaDeProdutosDisponiveis = new ArrayList();
 
-public String getId() {
-	return id;
-}
-
-public void setId(String id) {
-	this.id = id;
-}
-
-public String getNomeProduto() {
-	return nomeProduto;
-}
-
-public void setNomeProduto(String nomeProduto) {
-	this.nomeProduto = nomeProduto;
-}
-
-public double getValorProduto() {
-	return valorProduto;
-}
-
-public void setValorProduto(double valorProduto) {
-	this.valorProduto = valorProduto;
-}
-public void cadastrarProdutos() {
-	listaDeProdutosDisponiveis.add(1);
-	Produto produto = new Produto();
-	for (int i=0; i > listaDeProdutosDisponiveis.size(); i++) {
-	produto.setNomeProduto(JOptionPane.showInputDialog("Insira o nome do produto"));
-	produto.setId(JOptionPane.showInputDialog("Insira o id do produto"));
-	produto.setValorProduto(Double.parseDouble(JOptionPane.showInputDialog("Insira o valor do produto")));
-	listaDeProdutosDisponiveis.add(produto);
-	listaDeProdutosDisponiveis.remove(0);
-	int escolha = JOptionPane.showConfirmDialog(null,"Deseja adicionar outro produto ao estoque?");
-	JOptionPane.showMessageDialog(null,"Numero id :"+ escolha);
-	if(escolha == 0) {
-		continue;
-	}
-    else 
-    	break;
-	}
-	
-    
-}
-public void listarProdutos() {
-	
-	for(int i=0; i< listaDeProdutosDisponiveis.size(); i++ ) {
+	public Produto() {
 		
-		JOptionPane.showMessageDialog(null,listaDeProdutosDisponiveis.get(i));
 	}
-}
 
-@Override
-public String toString() {
-	return "Produto [id=" + id + ", nomeProduto=" + nomeProduto + ", valorProduto=" + valorProduto ;
-}
+	public String getId() {
+		return id;
+	}
 
-public void carrinhoDeCompras() {
-	
-}
+	public void setId(String id) {
+		this.id = id;
+	}
 
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+
+	public double getValorProduto() {
+		return valorProduto;
+	}
+
+	public void setValorProduto(double valorProduto) {
+		this.valorProduto = valorProduto;
+	}
+
+	public void cadastrarProdutos() {
+		boolean sair = true;
+		int cont=0;
+		boolean escolhaSair = true;
+		while(sair&& escolhaSair) {
+			
+			String nome = JOptionPane.showInputDialog("Insira o nome do produto");
+		    String id = JOptionPane.showInputDialog("Insira o id do produto");
+			double valor = Double.parseDouble(JOptionPane.showInputDialog("Insira o valor do produto"));
+			Produto produto = new Produto(nome,id,valor);
+			listaDeProdutosDisponiveis.add(produto);
+			
+			
+			int escolha = JOptionPane.showConfirmDialog(null, "Deseja adicionar outro produto ao estoque?");
+			
+			if(escolha == 0) {
+				sair = true;
+				cont = cont+1;
+			}else {
+				sair = false;
+			}
+		}
+		listarProdutos(listaDeProdutosDisponiveis,cont+1);
+
+	}
+
+	public Produto(String id, String nomeProduto, double valorProduto) {
+		this.id = id;
+		this.nomeProduto = nomeProduto;
+		this.valorProduto = valorProduto;
+		
+	}
+
+	public void listarProdutos(List produtos, int qtdProdutos) {
+
+		for (int i = 0; i <qtdProdutos; i++) {
+
+			JOptionPane.showMessageDialog(null, produtos.get(i).toString());
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "Produto [id=" + id + ", nomeProduto=" + nomeProduto + ", valorProduto=" + valorProduto;
+	}
+
+	public void carrinhoDeCompras() {
+
+	}
 
 }
